9d192b611b7194b1ed411eef7161d7fc9114ac81
rschuessler <roderick.schuessler8@googlemail.com>
2018-01-09 14:09:14 +0100

    bla

---
+++ src/pages/login/login.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(private navCtrl: NavController, private navParams: NavParams) {
+  }
+
+  navigateToPage(pageName: string){
+    pageName === "InboxPage" ? this.navCtrl.setRoot(pageName) : this.navCtrl.push(pageName);
+  }
+}
